6039c77ccc9359bbcf8a80e7329c55b06e32709e
Probirochniy <afl_damir@mail.ru>
2024-04-13 00:45:47 +0300

    [auth] Apply suggestions

--- api/auth/auth.py
+++ api/auth/auth.py
@@ -13,12 +13,8 @@ cookie_transport = CookieTransport(cookie_max_age=3600)
 SECRET = os.getenv("SECRET", "default_secret")
 
 
-def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
-
-
 auth_backend = AuthenticationBackend(
     name="jwt",
     transport=cookie_transport,
-    get_strategy=get_jwt_strategy,
+    get_strategy= lambda: JWTStrategy(secret=SECRET, lifetime_seconds=3600),
 )
