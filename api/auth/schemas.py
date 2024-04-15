c4b95a9fa3362c21958a3dc257bde06999d8466d
Damir Afliatonov <88425319+Probirochniy@users.noreply.github.com>
2024-04-15 15:34:15 +0300

    Update schemas.py

--- api/auth/schemas.py
+++ api/auth/schemas.py
@@ -1,8 +1,6 @@
 from fastapi_users import schemas
 from pydantic.version import VERSION as PYDANTIC_VERSION
 
-PYDANTIC_V2 = PYDANTIC_VERSION.startswith("2.")
-
 
 class UserRead(schemas.BaseUser[int]):
     pass
