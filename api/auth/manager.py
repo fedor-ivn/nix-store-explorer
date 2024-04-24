4b34ac78902030e7488f8ab2f2e3e4a3168224a4
Probirochniy <afl_damir@mail.ru>
2024-04-25 02:09:25 +0300

    [auth] Format

--- api/auth/manager.py
+++ api/auth/manager.py
@@ -14,17 +14,21 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
 
+
 class PasswordHelper(PasswordHelperProtocol):
     def hash(self, password: str) -> str:
         return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-    
+
     def generate(self) -> str:
         return bcrypt.gensalt().decode()
-    
+
     def verify_and_update(
         self, plain_password: str, hashed_password: str
     ) -> Tuple[bool, str]:
-        return bcrypt.checkpw(plain_password.encode(), hashed_password.encode()), hashed_password
+        return (
+            bcrypt.checkpw(plain_password.encode(), hashed_password.encode()),
+            hashed_password,
+        )
 
 
 async def get_user_manager(user_db=Depends(get_user_db)):
