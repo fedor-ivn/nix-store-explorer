81b3391ae9bcbf8308d347b4f45751bfd45cf5c0
Probirochniy <afl_damir@mail.ru>
2024-04-27 06:08:33 +0300

    [tests] more store router tests

--- tests/unit/test_store_endpoints.py
+++ tests/unit/test_store_endpoints.py
@@ -1,14 +1,21 @@
 import asyncio
 import os
 import shutil
-
 import pytest
 from fastapi.testclient import TestClient
+from unittest.mock import patch
+
+from src.store.schemas.package import (
+    ClosuresDifference,
+    Package,
+    PackageMeta,
+)
 
 os.environ["DATABASE_URL"] = "sqlite+aiosqlite:///test.db"
 
 from src.app import app  # noqa: E402
 from src.db.db import create_db_and_tables  # noqa: E402
+
 
 
 @pytest.fixture
@@ -119,48 +126,84 @@ def test_delete_store_not_found(client):
     assert response.json() == {"detail": "Store store was not found!"}
 
 
-# def test_add_package(client):
-#     client.post("/store/store", json={})
+def test_add_package(client):
+    with patch("src.store.router.StoreService.add_package") as mock_add_package:
+        mock_add_package.return_value = Package(
+            id=1,
+            name="package",
+            store_id=1,
+            closure={"packages": []},
+        )
 
-#     response = client.post("/store/store/package/package", json={})
+        response = client.post("/store/store/package/package", json={})
 
-#     print(response.json())
-#     assert response.status_code == 200
-#     assert response.json() == {
-#         "id": 1,
-#         "name": "package",
-#         "store_id": 1,
-#         "closure": {"packages": []},
-#     }
-
-
-# def test_delete_package(client):
-#     client.post("/store/store", json={})
-
-#     client.post("/store/store/package/package", json={})
-
-#     response = client.delete("/store/store/package/package")
-
-#     assert response.status_code == 200
-#     assert response.json() == {
-#         "id": 1,
-#         "name": "package",
-#         "store_id": 1,
-#         "closure": {"packages": []},
-#     }
+        assert response.status_code == 200
+        assert response.json() == {
+            "id": 1,
+            "name": "package",
+            "store_id": 1,
+            "closure": {"packages": []},
+        }
 
 
-# def test_get_paths_difference(client):
-#     response = client.get("/store/store1/difference/store2")
+def test_delete_package(client):
+    with patch("src.store.router.StoreService.delete_package") as mock_delete_package:
+        mock_delete_package.return_value = Package(
+            id=1,
+            name="package",
+            store_id=1,
+            closure={"packages": []},
+        )
 
-#     assert response.status_code == 200
-#     assert response.json() == {"absent_in_store_1": [], "absent_in_store_2": []}
+        response = client.delete("/store/store/package/package")
+
+        assert response.status_code == 200
+        assert response.json() == {
+            "id": 1,
+            "name": "package",
+            "store_id": 1,
+            "closure": {"packages": []},
+        }
 
 
-# def test_get_closures_difference(client):
-#     response = client.get(
-#         "/store/store1/package/package1/closure-difference/store2/package2"
-#     )
+def test_get_paths_difference(client):
+    with patch("src.store.router.StoreService.get_paths_difference") as mock_get_paths_difference:
+        mock_get_paths_difference.return_value = (
+            ["store1"],
+            ["store2"],
+        )
 
-#     assert response.status_code == 200
-#     assert response.json() == {"absent_in_closure_1": [], "absent_in_closure_2": []}
+        response = client.get("/store/store1/difference/store2")
+
+        assert response.status_code == 200
+        assert response.json() == {"absent_in_store_1": ["store2"], "absent_in_store_2": ["store1"]}
+
+
+def test_get_closures_difference(client):
+    with patch("src.store.router.StoreService.get_closures_difference") as mock_get_closures_difference:
+        mock_get_closures_difference.return_value = ClosuresDifference(
+            absent_in_package_1=["package2"],
+            absent_in_package_2=["package1"],
+        )
+
+        response = client.get("/store/store/package/package/closure-difference/store/package")
+
+        assert response.status_code == 200
+        assert response.json() == {
+            "absent_in_package_1": ["package2"],
+            "absent_in_package_2": ["package1"],
+        }
+
+
+def test_get_package_meta(client):
+    with patch("src.store.router.StoreService.get_package_meta") as mock_get_package_meta:
+        mock_get_package_meta.return_value = PackageMeta(
+            present=True,
+            closure_size=0,
+        )
+
+        response = client.get("/store/store/package/package")
+
+        assert response.status_code == 200
+        assert response.json() == {"present": True, "closure_size": 0}
+
